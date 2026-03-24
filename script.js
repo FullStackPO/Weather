484022e495257f406b1d827e41a1e6b4c45717a5
Harsh Arya <harsharya8946@gmail.com>
2026-03-24 23:03:52 +0530

    complete weather app

---
+++ script.js
@@ -0,0 +1,48 @@
+let api_key = `your_api_key`;
+
+let place = document.querySelector('#location');
+let temp = document.querySelector('#temp');
+let wind = document.querySelector('#wind');
+let humidity = document.querySelector('#humidity');
+let feels = document.querySelector('#feels');
+let desc = document.querySelector('#desc');
+let ecity = document.querySelector('#ecity');
+let btn = document.querySelector('#getInfo');
+
+async function weather(city) {
+    try {
+        place.innerText = "Loading...";
+
+        let raw = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`);
+
+        if (raw.ok) {
+            let data = await raw.json();
+
+            place.innerText = data.name;
+            temp.innerText = `${data.main.temp}°C`;
+            wind.innerText = data.wind.speed + " km/h";
+            feels.innerText = data.main.feels_like + "°C";
+            humidity.innerText = data.main.humidity + "%";
+            desc.innerText = data.weather[0].main;
+        } 
+        else {
+            alert(`No place found named "${city}"`);
+        }
+
+    } catch (err) {
+        console.log("Error:", err.message);
+    }
+}
+
+weather('delhi');
+
+function getWeather() {
+    let city = ecity.value.trim();
+
+    if (city === "") {
+        alert("Please enter a city name");
+        return;
+    }
+
+    weather(city);
+}
